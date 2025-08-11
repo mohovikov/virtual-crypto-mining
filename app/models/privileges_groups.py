ad221c624f4874a8214e0db2b2093aa7975eddc8
MOHOVICK <mohovikov.n@yandex.by>
2025-08-11 22:44:49 +0300

    Небольшие правки в модели PrivilegesGroups

--- app/models/privileges_groups.py
+++ app/models/privileges_groups.py
@@ -10,13 +10,13 @@ class PrivilegesGroups(db.Model):
     __tablename__ = "privileges_groups"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(String(30), nullable=False, unique=True, index=True)
-    privileges: Mapped[int] = mapped_column(Integer, nullable=False)
+    name: Mapped[Optional[str]] = mapped_column(String(30), nullable=False, unique=True, index=True)
+    privileges: Mapped[Optional[int]] = mapped_column(Integer, nullable=False)
     color_class: Mapped[Optional[str]] = mapped_column(String(30), nullable=True)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
-    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
 
-    def __init__(self, name: str, privileges: int, color_class: str | None) -> None:
+    def __init__(self, name: str | None, privileges: int | None, color_class: str | None) -> None:
         self.name = name
         self.privileges = privileges
         self.color_class = color_class
