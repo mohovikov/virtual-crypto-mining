f8e40a88d571719edc0eff6217222f67a6859985
MOHOVICK <mohovikov.n@yandex.by>
2025-08-11 11:58:43 +0300

    Инициализация библиотек flask-login и flask-bcrypt

--- app/extensions.py
+++ app/extensions.py
@@ -1,10 +1,14 @@
 from flask_apscheduler import APScheduler
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_redis import FlaskRedis
 from flask_sqlalchemy import SQLAlchemy
 
 
+bcrypt = Bcrypt()
 db = SQLAlchemy()
+login_manager = LoginManager()
 migrate = Migrate()
 redis_client = FlaskRedis()
 scheduler = APScheduler()
