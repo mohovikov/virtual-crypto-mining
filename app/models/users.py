5d98119ea8882dc6cc05ab38418bbe31ac806675
MOHOVICK <mohovikov.n@yandex.by>
2025-08-11 11:58:07 +0300

    Fix username_aka, наследство от UserMixin, добавлен user_loader для flask-login

--- app/models/users.py
+++ app/models/users.py
@@ -1,16 +1,18 @@
 from datetime import datetime, timezone
 from typing import Optional
+from flask_login import UserMixin
 from sqlalchemy import Integer, String, Text, DateTime, CHAR
 from sqlalchemy.orm import Mapped, mapped_column
-from app.extensions import db
+
+from app.extensions import db, login_manager
 
 
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, index=True)
     username: Mapped[str] = mapped_column(String(30), nullable=False, unique=True, index=True)
-    username_aka: Mapped[str] = mapped_column(String(32), nullable=False)
+    username_aka: Mapped[str] = mapped_column(String(32), nullable=True)
     userpage: Mapped[str] = mapped_column(Text, nullable=True)
     email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
@@ -23,3 +25,8 @@ class Users(db.Model):
         self.username = username
         self.email = email
         self.password_hash = password_hash
+
+
+@login_manager.user_loader
+def load_user(uid):
+    return Users.query.get(uid)
