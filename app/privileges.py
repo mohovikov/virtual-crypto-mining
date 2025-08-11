3810108f904e3785ec7e2685cf03fba25ccc90a4
MOHOVICK <mohovikov.n@yandex.by>
2025-08-12 00:38:03 +0300

    те самые 4 переменные и новая функция)

--- app/privileges.py
+++ app/privileges.py
@@ -4,11 +4,15 @@ from flask_login import current_user
 
 @unique
 class Privileges(IntEnum):
-    USER_PUBLIC = 1 << 0
-    USER_NORMAL = 2 << 0
-    USER_SPONSOR = 2 << 1
-    ADMIN_ACCESS_PANEL = 2 << 2
-    ADMIN_MANAGE_USERS = 2 << 3
+    USER_PUBLIC             = 1 << 0
+    USER_NORMAL             = 2 << 0
+    USER_SPONSOR            = 2 << 1
+    ADMIN_ACCESS_PANEL      = 2 << 2
+
+    ADMIN_EDIT_USERS        = 2 << 3
+    ADMIN_RESTRICT_USERS    = 2 << 4
+    ADMIN_BAN_USERS         = 2 << 5
+    ADMIN_LOCK_USERS        = 2 << 6
 
 def has_privilege(privileges: Privileges, user_privileges: int = -1) -> bool:
     if user_privileges == -1:
@@ -17,6 +21,12 @@ def has_privilege(privileges: Privileges, user_privileges: int = -1) -> bool:
         user_privileges = current_user.privileges
 
     return privileges & user_privileges != 0
+
+def has_any_privilege(privileges_list: list[Privileges], user_privileges: int = -1) -> bool:
+    for priv in privileges_list:
+        if has_privilege(priv, user_privileges):
+            return True
+    return False
 
 def is_locked(privileges: int = -1) -> bool:
     """Заблокирован: нет PUBLIC и нет NORMAL."""
