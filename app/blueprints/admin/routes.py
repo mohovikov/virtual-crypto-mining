9c0d4b5586ffa29f00cfcfe87ccc8b9bda53d1b0
MOHOVICK <mohovikov.n@yandex.by>
2025-08-11 18:36:18 +0300

    Новый route со списком всех пользователей

--- app/blueprints/admin/routes.py
+++ app/blueprints/admin/routes.py
@@ -1,8 +1,16 @@
-from flask import render_template
+from flask import render_template, request
 
+from app import services
 from app.blueprints.admin import admin
 
 
-@admin.route("/")
+@admin.route("/dashboard")
 def dashboard():
     return render_template("admin/dashboard.html")
+
+@admin.route("/users")
+def users():
+    return render_template(
+        "admin/users.html",
+        users = services.get_all_users(request.args.get("id", type=int, default=1))
+    )
