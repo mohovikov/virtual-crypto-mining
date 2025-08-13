2a3a26f8715439de9afd6baa4d5207d2bb833391
MOHOVICK <mohovikov.n@yandex.by>
2025-08-13 10:33:36 +0300

    Небольшие правки кода

--- static/js/privileges.js
+++ static/js/privileges.js
@@ -3,33 +3,20 @@ function updatePrivileges() {
   $("input:checkbox[name=privilege]:checked").each(function(){
     result = Number(result) + Number($(this).val())
   })
-
-  var UserSponsor = 4
-  var UserNormal = 2
-  var UserPublic = 1
-
-  // Remove donor if needed
-  var selectValue
-  if (result != UserSponsor | UserNormal | UserPublic) {
-    selectValue = result & ~UserSponsor
-  } else {
-    selectValue = result
-  }
-
   $("#privileges_value").val(result)
-  $("#privileges-group").val(selectValue)
+  $("#privileges-group").val(result)
 }
 
 function groupUpdated() {
   var privileges = $("#privileges-group option:selected").val()
   if (privileges > -1) {
     $("input:checkbox[name=privilege]").each(function(){
-      if ( ($(this).val() & privileges) > 0) {
+      if (($(this).val() & privileges) > 0) {
         $(this).prop("checked", true)
       } else {
         $(this).prop("checked", false)
       }
     })
   }
-  updatePrivileges();
+  updatePrivileges()
 }
