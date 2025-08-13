31179d9698dce7524aaf3069da6657a1dbb47f2e
MOHOVICK <mohovikov.n@yandex.by>
2025-08-13 10:27:31 +0300

    Функция выдачи спонсорства

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,10 +2,14 @@ from .privileges import (
     get_all_privileges,
     add_privilege_group
 )
+from .sponsor import (
+    give_sponsor
+)
 from .users import (
     create_user,
     login_user,
-    get_all_users
+    get_all_users,
+    get_user_data
 )
 
 __all__ = [
@@ -13,8 +17,11 @@ __all__ = [
     "get_all_privileges",
     "add_privilege_group",
 
+    "give_sponsor",
+
     # Users
     "create_user",
     "login_user",
-    "get_all_users"
+    "get_all_users",
+    "get_user_data",
 ]
---
+++ app/services/sponsor.py
@@ -0,0 +1,22 @@
+from markupsafe import Markup
+
+from app.extensions import db
+from app.helpers import add_sponsor_time
+from app.models.users import Users
+from app.privileges import Privileges
+from app.forms.admin_forms import GiveSponsorForm
+
+
+def give_sponsor(form: GiveSponsorForm, user: Users):
+    try:
+        user.sponsor_expire = add_sponsor_time(user.sponsor_expire, form.amount.data, form.unit.data)
+        user.privileges |= Privileges.USER_SPONSOR
+        db.session.commit()
+        return True, Markup(
+            f"Пользователю {user.username} выдан спонсор до "
+            f"<time class=\"js-datetime\" data-utc=\"{user.sponsor_expire}\"></time>"
+        ), "success"
+    except Exception as ex:
+        db.session.rollback()
+        print(ex)
+        return False, f"Ошибка при выдаче спонсорства: {ex}", "danger"
