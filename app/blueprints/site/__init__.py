6cd05ecf8392d4e003b3f5ccfb88153578ed924a
MOHOVICK <mohovikov.n@yandex.by>
2025-08-17 00:05:17 +0300

    Включение blueprint'a clan в blueprint site

--- app/blueprints/site/__init__.py
+++ app/blueprints/site/__init__.py
@@ -1,5 +1,8 @@
 from flask import Blueprint
 
+from app.blueprints.site.clan import clan
+
 site = Blueprint("site", __name__, url_prefix="/")
+site.register_blueprint(clan)
 
 from app.blueprints.site import routes
