f50ac40ed265b972c133ac35b912e1d0109f3fa1
MOHOVICK <mohovikov.n@yandex.by>
2025-08-18 00:07:10 +0300

    Форма редактирования клана, исправление ошибок

--- app/forms/site_forms.py
+++ app/forms/site_forms.py
@@ -1,51 +1,62 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, PasswordField, StringField, SubmitField, ValidationError
+import wtforms as forms
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 from app.models.users import Users
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(
+    username = forms.StringField(
         "Имя пользователя",
         validators=[DataRequired(), Length(min=3, max=20)]
     )
-    email = StringField(
+    email = forms.StringField(
         "Email",
         validators=[DataRequired(), Email()]
     )
-    password = PasswordField(
+    password = forms.PasswordField(
         "Пароль",
         validators=[DataRequired(), Length(min=6)]
     )
-    confirm_password = PasswordField(
+    confirm_password = forms.PasswordField(
         "Подтверждение пароля",
         validators=[DataRequired(), EqualTo("password")]
     )
-    submit = SubmitField("Зарегистрироваться")
+    submit = forms.SubmitField("Зарегистрироваться")
 
     def validate_username(self, username):
         if Users.query.filter_by(username=username.data).first():
-            raise ValidationError("Это имя уже занято.")
+            raise forms.ValidationError("Это имя уже занято.")
 
     def validate_email(self, email):
         if Users.query.filter_by(email=email.data).first():
-            raise ValidationError("Этот email уже зарегистрирован.")
+            raise forms.ValidationError("Этот email уже зарегистрирован.")
 
 class LoginForm(FlaskForm):
-    username = StringField(
+    username = forms.StringField(
         "Имя пользователя",
         validators=[DataRequired(), Length(min=3, max=20)]
     )
-    password = PasswordField(
+    password = forms.PasswordField(
         "Пароль",
         validators=[DataRequired()]
     )
-    remember = BooleanField("Запомнить меня")
-    submit = SubmitField("Войти")
+    remember = forms.BooleanField("Запомнить меня")
+    submit = forms.SubmitField("Войти")
 
 class ClanCreateForm(FlaskForm):
-    name = StringField("Название клана", validators=[DataRequired(), Length(max=255)], render_kw={"class": "form-control"})
-    short_name = StringField("Тэг клана", validators=[DataRequired(), Length(max=255)], render_kw={"class": "form-control"})
-    is_open = BooleanField("Открытый набор", default=True)
-    submit = SubmitField("Создать клан", render_kw={"class": "btn btn-sm btn-primary"})
+    name = forms.StringField("Название клана", validators=[DataRequired(), Length(max=255)], render_kw={"class": "form-control"})
+    short_name = forms.StringField("Тэг клана", validators=[DataRequired(), Length(max=255)], render_kw={"class": "form-control"})
+    is_open = forms.BooleanField("Открытый набор", default=True)
+    submit = forms.SubmitField("Создать клан", render_kw={"class": "btn btn-sm btn-primary"})
+
+class ClanSettingsForm(FlaskForm):
+    name = forms.StringField("Название клана", render_kw={"class": "form-control"})
+    short_name = forms.StringField("Тэг клана", render_kw={"class": "form-control"})
+    is_open = forms.RadioField("Открытый набор", choices=[('1', 'Да'), ('0', 'Нет')])
+    logo_file = FileField("Загрузка аватарки", render_kw={"class": "form-control"})
+    banner_file = FileField("Загрузка обложки", render_kw={"class": "form-control"})
+    url = forms.URLField("Ссылка клана", render_kw={"class": "form-control"})
+    description = forms.TextAreaField("Описание клана", render_kw={"class": "form-control"})
+    submit = forms.SubmitField("Сохранить изменения", render_kw={"class": "btn btn-sm btn-primary"})
