dfeae9075a1a19238c1d812514cd0010e96d7800
MOHOVICK <mohovikov.n@yandex.by>
2025-08-18 00:04:36 +0300

    Аналогия static для media

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import os
+from flask import Flask, abort, send_from_directory
 
 from app import helpers, services
 from app.constants import Privileges
@@ -27,6 +28,16 @@ def create_app() -> Flask:
             get_clan_link=helpers.get_clan_link,
             get_privilege_group=services.get_privilege_group
         )
+
+    @app.route("/media/<path:filename>")
+    def media(filename):
+        safe_path = os.path.join(Config.MEDIA_FOLDER, filename)
+        if not os.path.isfile(safe_path):
+            abort(404)
+
+        dir_name = os.path.dirname(safe_path)
+        file_name = os.path.basename(safe_path)
+        return send_from_directory(directory=dir_name, path=file_name)
 
     app.config.from_object(Config)
     db.init_app(app)
