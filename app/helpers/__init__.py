29b0f129a14f893e17be3107d265566c795ab735
MOHOVICK <mohovikov.n@yandex.by>
2025-08-19 12:58:12 +0300

    Изменения кода

--- app/helpers/__init__.py
+++ app/helpers/__init__.py
@@ -1,6 +1,3 @@
-from .clan_helper import (
-    get_clan_link
-)
 from .file_helper import (
     save_clan_hashed_file
 )
@@ -13,8 +10,6 @@ from .template_helper import (
 )
 
 __all__ = [
-    "get_clan_link",
-
     "save_clan_hashed_file",
 
     "add_sponsor_time",
