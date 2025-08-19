8973f069b158e54314a6400e464f8cb73c839969
MOHOVICK <mohovikov.n@yandex.by>
2025-08-19 12:27:09 +0300

    Загрузка переменных из env

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from dotenv import find_dotenv, load_dotenv
+load_dotenv(find_dotenv())
+
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
