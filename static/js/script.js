941047e6fd7fbddb1bcd7397d9140120ef08a135
MOHOVICK <mohovikov.n@yandex.by>
2025-08-21 14:13:51 +0300

    Автоматическое обновление введеных данных в превью

--- static/js/script.js
+++ static/js/script.js
@@ -28,3 +28,17 @@ document.querySelectorAll('time.js-timeago').forEach(el => {
 
   el.textContent = format(date, 'ru')
 })
+
+$(function() {
+  function updateOutput() {
+    const bbcode = $("#userpage").val()
+    const html = bbcodeToHtml(bbcode)
+    $("#userpage-preview").html(html)
+  }
+
+  // Первичная инициализация
+  updateOutput()
+
+  // Автообновление при вводе
+  $("#userpage").on("input", updateOutput)
+});
