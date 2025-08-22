d79fbd687c7c969e6aefb9563790e7a58c1fc62e
MOHOVICK <mohovikov.n@yandex.by>
2025-08-23 00:40:11 +0300

    Подключение новых таблиц для работы и миграций

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,11 +1,18 @@
 from .clan import Clan
 from .clan_member import ClanMember
+from .crypto_price_history import CryptoPriceHistory
+from .cryptocurrency import Cryptocurrency
 from .privileges_groups import PrivilegesGroup
+from .user_balance import UserBalance
 from .users import User
+
 
 __all__ = [
     "Clan",
+    "CryptoPriceHistory",
     "ClanMember",
+    "Cryptocurrency",
     "PrivilegesGroup",
-    "User"
+    "User",
+    "UserBalance"
 ]
