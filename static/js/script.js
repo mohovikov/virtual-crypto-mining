118d8cb9347b2f6f2304dea052580ce1fd2545e9
MOHOVICK <mohovikov.n@yandex.by>
2025-08-23 00:40:42 +0300

    Красивое отображение стоимости, функция создания графика

--- static/js/script.js
+++ static/js/script.js
@@ -29,6 +29,15 @@ document.querySelectorAll('time.js-timeago').forEach(el => {
   el.textContent = format(date, 'ru')
 })
 
+document.querySelectorAll('span.js-price').forEach(el => {
+  const price = parseFloat(el.dataset.price)
+
+  el.textContent = price.toLocaleString("ru-RU", {
+    style: "currency",
+    currency: "RUB"
+  })
+})
+
 $(function() {
   function updateOutput() {
     const bbcode = $("#userpage").val()
@@ -41,4 +50,50 @@ $(function() {
 
   // Автообновление при вводе
   $("#userpage").on("input", updateOutput)
-});
+})
+
+async function loadChart(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+
+  const labels = data.map(item => {
+    let isoStr = item.timestamp.replace(' ', 'T').split('.')[0] + 'Z'
+    let date = new Date(isoStr)
+    let options = {
+      year: 'numeric', month: '2-digit', day: '2-digit',
+      hour: '2-digit', minute: '2-digit', second: '2-digit'
+    }
+    return date.toLocaleString(undefined, options)
+  })
+  const prices = data.map(item => item.price)
+
+  const ctx = document.getElementById("priceChart").getContext("2d")
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [{
+        label: "Цена",
+        data: prices,
+        borderColor: "blue",
+        borderWidth: 2,
+        fill: false,
+        tension: 0.1
+      }]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: { display: true },
+        title: {
+          display: true,
+          text: 'График курса валюты'
+        }
+      },
+      scales: {
+        x: { title: { display: true, text: "Время" }},
+        y: { title: { display: true, text: "Стоимость" }}
+      }
+    }
+  })
+}
