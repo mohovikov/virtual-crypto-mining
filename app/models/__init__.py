58a3dc4e2096f0c22b8a61098da79630596643ce
MOHOVICK <mohovikov.n@yandex.by>
2025-08-24 16:30:39 +0300

    Новая таблица логов задач

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,6 +3,7 @@ from .clan_member import ClanMember
 from .crypto_price_history import CryptoPriceHistory
 from .cryptocurrency import Cryptocurrency
 from .privileges_groups import PrivilegesGroup
+from .scheduler_log import SchedulerLog
 from .user_balance import UserBalance
 from .users import User
 
@@ -13,6 +14,7 @@ __all__ = [
     "ClanMember",
     "Cryptocurrency",
     "PrivilegesGroup",
+    "SchedulerLog",
     "User",
     "UserBalance"
 ]
