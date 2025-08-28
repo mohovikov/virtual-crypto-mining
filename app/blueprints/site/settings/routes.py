9d1e69b3d454c788bf1670803c250cd6fee5216b
MOHOVICK <mohovikov.n@yandex.by>
2025-08-28 22:27:04 +0300

    Улучшение кода, POST запросы для настроек аватарки и заднего фона профиля, отображение последнего, если таковой имеется

--- app/blueprints/site/settings/routes.py
+++ app/blueprints/site/settings/routes.py
@@ -1,23 +1,49 @@
-from flask import render_template
+from flask import flash, render_template
 
+import app.services.site as services
 from app.blueprints.site.settings import settings
+from app.forms.site import SettingsForm
 
 
 @settings.route("/")
 def profile():
-    return render_template("site/settings/profile.html")
+    form = SettingsForm()
+
+    return render_template(
+        "site/settings/profile.html",
+        form = form
+    )
 
 @settings.route("/userpage")
 def userpage():
-    return render_template("site/settings/userpage.html")
+    form = SettingsForm()
 
-@settings.route("/avatar")
+    return render_template(
+        "site/settings/userpage.html",
+        form = form
+    )
+
+@settings.route("/avatar", methods=['GET', 'POST'])
 def avatar():
-    return render_template("site/settings/avatar.html")
+    form = SettingsForm()
+
+    if form.validate_on_submit() and form.avatar_file.data:
+        message, category = services.save_user_avatar(form)
+
+        flash(message, category)
+
+    return render_template(
+        "site/settings/avatar.html",
+        form = form
+    )
 
 @settings.route("/password")
 def password():
-    return render_template("site/settings/password.html")
+    form = SettingsForm()
+
+    return render_template(
+        "site/settings/password.html"
+    )
 
 @settings.route("/cryptocurrency")
 def cryptocurrency():
@@ -27,6 +53,16 @@ def cryptocurrency():
 def badge():
     return render_template("site/settings/badge.html")
 
-@settings.route("/background")
+@settings.route("/background", methods=['GET', 'POST'])
 def background():
-    return render_template("site/settings/background.html")
+    form = SettingsForm()
+
+    if form.validate_on_submit() and form.background_file.data:
+        message, category = services.save_user_background(form)
+
+        flash(message, category)
+
+    return render_template(
+        "site/settings/background.html",
+        form = form
+    )
