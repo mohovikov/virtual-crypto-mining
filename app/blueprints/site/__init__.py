3c8deb3f99707ff28930ff84a8ce6af91741ba50
MOHOVICK <mohovikov.n@yandex.by>
2025-08-28 17:56:03 +0300

    Подключение blueprint с настройками

--- app/blueprints/site/__init__.py
+++ app/blueprints/site/__init__.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
 
+from app.blueprints.site.settings import settings
+
 
 site = Blueprint("site", __name__, url_prefix = "/")
+site.register_blueprint(settings)
 
 from app.blueprints.site import routes
