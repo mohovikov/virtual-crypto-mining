bbbc56b68863a4271d1f9a46eb219d31ef3b77cf
MOHOVICK <mohovikov.n@yandex.by>
2025-08-28 17:53:35 +0300

    Функция получения пользователя по ID

--- app/services/site/__init__.py
+++ app/services/site/__init__.py
@@ -1,10 +1,12 @@
 from .user_services import (
+    get_user_by_id,
     register_user,
     login_user
 )
 
 
 __all__ = [
+    "get_user_by_id",
     "register_user",
     "login_user"
 ]
--- app/services/site/user_services.py
+++ app/services/site/user_services.py
@@ -4,6 +4,9 @@ from app.forms.site import RegisterForm, LoginForm
 from app.models import User
 
 
+def get_user_by_id(user_id: int) -> User:
+    return User.query.get_or_404(user_id)
+
 def register_user(form: RegisterForm):
     user = User(
         username = str(form.username.data),
